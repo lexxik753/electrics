0ad4d493077a7e1b30dc43ba126b487034dad3dd
Alexey Gurov <lexxik753@gmail.com>
2025-04-30 21:07:41 +0300

    create button, before update tailwind

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -30,8 +30,9 @@ const showMenu = ref(false);
 <template>
     <header class="flex bg-[#18212b] text-white">
         <nav class="flex grow items-center md:flex md:grow md:items-center">
-            <div>
-                <img class="m-auto" src="../assets/media/logo.png" alt="логотип электромонтажа">
+            <div class="w-32 md:w-40">
+                <img class="w-full h-auto object-contain m-auto" src="../assets/media/logo.png"
+                    alt="логотип электромонтажа" width="160" height="48">
             </div>
             <ul class="hidden md:flex lg:flex-row">
                 <li class="px-6" v-for="(link, index) in menu" :key="index"><a :href="link.link">{{ link.title }}</a>
@@ -46,8 +47,7 @@ const showMenu = ref(false);
                 <path d="M6 22.0002L26 22.0008" />
             </svg>
         </button>
-        <div class="bg-[#18212b] md:hidden h-[100vh] w-full absolute top-0"
-            :class="showMenu == true ? 'block' : 'hidden'">
+        <div class="fixed inset-0 bg-[#18212b] md:hidden w-full z-50" :class="showMenu == true ? 'block' : 'hidden'">
             <div class="flex justify-end mt-4 mr-5">
                 <button @click="showMenu = false">X</button>
             </div>
@@ -62,11 +62,23 @@ const showMenu = ref(false);
         <div class="relative z-10">
             <h2 class="py-4 text-xl md:text-2xl text-center">ЭЛЕКТРОМОНТАЖ</h2>
             <h1 class="mb-4 text-2xl md:text-[52px] font-bold text-center">Электричество для вашего дома</h1>
-            <section class="m-auto w-1/2 text-center">
-                <p class="md:text-lg md:leading-loose">Мы предлагаем комплексные решения в области электротехники и
+            <section class="m-auto md:w-1/2 text-center">
+                <p class="text-sm md:text-lg md:leading-loose text-shadow-lg">Мы предлагаем комплексные решения в области
+                    электротехники и
                     сантехники,
                     учитывая ваши индивидуальные потребности. От начальной установки до регулярного обслуживания, мы
                     гарантируем профессионализм и качественную работу, которой вы можете доверять.</p>
+                <a href="#"
+                    class="bg-[#FF6B35] text-white mt-4 py-3 px-8 rounded-lg hover:bg-[#FF7F4D] transition-colors shadow-md hover:shadow-lg inline-flex items-center gap-2">
+                    <svg 
+                        class="w-5 h-5" 
+                        fill="currentColor" 
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true">
+                        <path d="M13 10h7l-9 13v-9H4l9-13v9Z"/>
+                    </svg>
+                    Узнать больше
+                </a>
             </section>
         </div>
     </div>
