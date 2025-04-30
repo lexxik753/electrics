440c9992e25a52bccc1cc1b3425fca852aa50a13
Alexey Gurov <lexxik753@gmail.com>
2025-04-30 15:39:00 +0300

    create burger menu with event, source fonts

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -23,25 +23,37 @@ const menu = ref([
         link: '#',
     },
 ])
+
+const showMenu = ref(false);
+
 </script>
 <template>
     <header class="flex bg-[#18212b] text-white">
-        <div class="flex m-auto">
-            <img src="../assets/media/logo.png" alt="логотип электромонтажа">
-        </div>
-        <nav class="flex grow md:items-center">
+        <nav class="flex grow items-center md:flex md:grow md:items-center">
+            <div class="grow md:grow-0">
+                <img class="m-auto" src="../assets/media/logo.png" alt="логотип электромонтажа">
+            </div>
             <ul class="hidden md:flex lg:flex-row">
                 <li class="px-6" v-for="(link, index) in menu" :key="index"><a :href="link.link">{{ link.title }}</a>
                 </li>
             </ul>
-            <button class="md:hidden ml-auto mr-3">
-                <svg class=" hover:stroke-2" width="32" height="32" viewBox="0 0 32 32" fill="none"
-                    xmlns="http://www.w3.org/2000/svg" stroke="white">
-                    <path d="M6 9.99927L26 9.99978" />
-                    <path d="M6 15.9998L26 16.0003" />
-                    <path d="M6 22.0002L26 22.0008" />
-                </svg>
-            </button>
         </nav>
+        <button class="block md:hidden ml-auto mr-3 relative" @click="showMenu = true">
+            <svg class=" hover:stroke-2" width="32" height="32" viewBox="0 0 32 32" fill="none"
+                xmlns="http://www.w3.org/2000/svg" stroke="white">
+                <path d="M6 9.99927L26 9.99978" />
+                <path d="M6 15.9998L26 16.0003" />
+                <path d="M6 22.0002L26 22.0008" />
+            </svg>
+        </button>
+        <div class="bg-[#18212b] md:hidden h-[100vh] w-full absolute top-0" :class="showMenu == true ? 'block' : 'hidden'">
+            <div class="flex justify-end mt-4 mr-5">
+                <button @click="showMenu = false">X</button>
+            </div>
+            <ul class="flex flex-col items-center space-y-2 ">
+                <li class="px-6" v-for="(link, index) in menu" :key="index"><a :href="link.link">{{ link.title }}</a>
+                </li>
+            </ul>
+        </div>
     </header>
 </template>
