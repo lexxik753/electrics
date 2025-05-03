a317d9d0887aa5e24623333ee666f581737ddb47
lexxik753 <lexxik753@gmail.com>
2025-05-03 20:37:06 +0300

    fix position section about

--- src/blocks/About.vue
+++ src/blocks/About.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <section class="flex lg:flex-row md:flex-col lg:justify-center md:items-center gap-4 my-6">
+    <section class="flex lg:flex-row md:flex-col lg:justify-center md:items-center gap-4 -my-10 relative z-50">
         <div class=" w-full lg:w-[45%] xl:w-[35%] 2xl:w-[30%] p-6 lg:p-8 font-sans rounded-lg bg-[#F2F8FC] lg:h-[400px]">
             <h3 class="text-2xl lg:text-3xl xl:text-4xl font-bold text-[#1A3D5F]">О нас</h3>
             <hr class="w-16 my-4 border-2 border-[#90C2E7] rounded-full">
