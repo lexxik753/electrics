86fcc617fe94a3a170db898474cd45cdd13102df
lexxik753 <lexxik753@gmail.com>
2025-05-04 13:19:28 +0300

    correct text about us

--- src/blocks/About.vue
+++ src/blocks/About.vue
@@ -2,12 +2,12 @@
 </script>
 
 <template>
-    <section class="flex lg:flex-row md:flex-col lg:justify-center md:items-center gap-4 -my-10 relative z-50">
+    <section class="flex lg:flex-row md:flex-col lg:justify-center md:items-center gap-4 -my-8 mx-4 relative">
         <div class=" w-full lg:w-[45%] xl:w-[35%] 2xl:w-[30%] p-6 lg:p-8 font-sans rounded-lg bg-[#F2F8FC] lg:h-[400px]">
             <h3 class="text-2xl lg:text-3xl xl:text-4xl font-bold text-[#1A3D5F]">О нас</h3>
             <hr class="w-16 my-4 border-2 border-[#90C2E7] rounded-full">
             <p class="text-base lg:text-lg xl:text-xl text-[#2D506D] leading-relaxed">
-                Наша команда специалистов по электромонтажным и сантехническим работам гарантирует безопасность, качество и надёжность. Мы стремимся к долгосрочному сотрудничеству и ценим каждого клиента.
+                Наша команда специалистов по электромонтажным работам гарантирует безопасность, качество и надёжность. Мы стремимся к долгосрочному сотрудничеству и ценим каждого клиента.
             </p>
         </div>
         <div class="w-full lg:w-[45%] xl:w-[35%] 2xl:w-[30%] h-[300px] lg:h-[400px] rounded-lg overflow-hidden shadow-lg">
