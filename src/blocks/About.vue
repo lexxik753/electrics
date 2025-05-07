ff3b4f7f908b0e78fd764244524d2602cbd441a2
Alexey Gurov <lexxik753@gmail.com>
2025-05-07 17:09:07 +0300

    fix responsive about, start create services

--- src/blocks/About.vue
+++ src/blocks/About.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-    <section class="flex lg:flex-row md:flex-col lg:justify-center md:items-center gap-4 m-5 relative">
-        <div class=" w-full lg:w-1/2 xl:w-1/4 2xl:w-[30%] lg:h-[400px] p-6 font-sans rounded-lg bg-[#F2F8FC]">
+    <section class="flex flex-col lg:flex-row lg:justify-center md:items-center gap-4 m-5 relative">
+        <div class=" w-full lg:w-1/2 xl:w-1/4 2xl:w-[30%] lg:h-[400px] p-6 font-sans bg-[#F2F8FC] rounded-lg">
             <h3 class="text-2xl lg:text-3xl xl:text-4xl font-bold text-[#1A3D5F]">О нас</h3>
             <hr class="w-16 my-4 border-2 border-[#90C2E7] rounded-full">
             <p class="text-base lg:text-lg xl:text-xl text-[#2D506D] leading-relaxed">
