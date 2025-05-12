a1e2dafdbd136adb448e143119dac68d9671fa93
Alex <lexxik753@gmail.com>
2025-05-12 12:58:15 +0300

    need done services details

--- src/blocks/Services.vue
+++ src/blocks/Services.vue
@@ -1,18 +1,46 @@
 <script setup>
+import ServicesDetails from '../components/ServicesDetails.vue';
+
+const servicesDetails = [
+    {
+        title: 'Электромонтажные работы',
+        price: 'от 500₽',
+    },
+    {
+        title: 'Ремонт',
+        price: 'от 500₽',
+    },
+    {
+        title: 'Комплексные электромонтажные работы',
+        price: 'Договорная',
+    },
+    {
+        title: 'Электромонтажные работы',
+        price: 'от 500₽',
+    },
+    {
+        title: 'Ремонт',
+        price: 'от 500₽',
+    },
+    {
+        title: 'Комплексные электромонтажные работы',
+        price: 'Договорная',
+    },
+]
 
 </script>
 
 <template>
-    <section class="mt-15">
+    <section class="container mt-15 mx-auto px-4">
         <div class="text-center">
             <h2 class="mb-2 text-xl md:text-2xl lg:text-4xl font-bold">Услуги, которые работают</h2>
             <p class="text-base lg:text-lg xl:text-2xl">Наша команда занимается электромонтажом и предлагает техническое
                 обслуживание, ремонт и консультации для стабильной работы систем</p>
         </div>
-        <div class="grid grid-cols-3">
-            <div class="bg-red-500">1</div>
-            <div class="bg-blue-500">2</div>
-            <div class="bg-green-500">3</div>
+        <div class="grid grid-cols-3 divide-x-2">
+            <ServicesDetails :details="servicesDetails"/>
+            <!-- <ServicesDetails/>
+            <ServicesDetails/> -->
         </div>
     </section>
 </template>
---
+++ src/components/ServicesDetails.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+    details: [
+        {
+            title: String,
+            price: String,
+            descriptions: String
+        }
+    ],
+})
+</script>
+
+<template>
+    <div v-for="(detail, index) in props.details" :key="index" class="mt-4 py-3 flex justify-center space-x-8">
+        <h3 class="text-lg font-bold mb-4 pb-1 border-b-2 border-orange-500 inline-block">{{ detail.title }}</h3>
+        <p>{{ detail.price }}</p>
+    </div>
+</template>
