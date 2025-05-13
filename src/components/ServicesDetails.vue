608e2d9d4ab26c91042e1699c39769378bab1796
Alex <lexxik753@gmail.com>
2025-05-13 14:46:45 +0300

    replace text

--- src/components/ServicesDetails.vue
+++ src/components/ServicesDetails.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 </script>
 <template>
     <div v-for="(detail, index) in props.details" :key="index" class="mt-4 p-2 flex flex-wrap justify-between items-baseline">
-        <h3 class="text-base font-bold mb-4 pb-1 border-b-2 border-orange-500 inline-block">{{ detail.title }}</h3>
+        <h4 class="text-base font-bold mb-4 pb-1 border-b-2 border-orange-500 inline-block">{{ detail.title }}</h4>
         <p>{{ detail.price }}</p>
         <p class="w-full text-xs">{{ detail.clarification }}</p>
     </div>
