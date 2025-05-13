c25cfa0695287716b75b1559b0fe40380f86790f
Alex <lexxik753@gmail.com>
2025-05-13 11:09:36 +0300

    need fix and create responsive block services

--- src/components/ServicesDetails.vue
+++ src/components/ServicesDetails.vue
@@ -9,9 +9,9 @@ const props = defineProps({
     ],
 })
 </script>
-
+<!-- last:col-span-3 last:justify-center  -->
 <template>
-    <div v-for="(detail, index) in props.details" :key="index" class="mt-4 py-3 flex justify-center space-x-8">
+    <div v-for="(detail, index) in props.details" :key="index" class="mt-4 p-3 flex justify-between items-baseline space-x-5">
         <h3 class="text-lg font-bold mb-4 pb-1 border-b-2 border-orange-500 inline-block">{{ detail.title }}</h3>
         <p>{{ detail.price }}</p>
     </div>
