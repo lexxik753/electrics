0199f4dd222f11416d69eb282c8620ee2ec864b4
Alex <lexxik753@gmail.com>
2025-05-13 14:08:57 +0300

    create clarification field

--- src/components/ServicesDetails.vue
+++ src/components/ServicesDetails.vue
@@ -4,15 +4,15 @@ const props = defineProps({
         {
             title: String,
             price: String,
-            descriptions: String
+            clarification: String
         }
     ],
 })
 </script>
-<!-- last:col-span-3 last:justify-center  -->
 <template>
-    <div v-for="(detail, index) in props.details" :key="index" class="mt-4 p-3 flex justify-between items-baseline space-x-5">
-        <h3 class="text-lg font-bold mb-4 pb-1 border-b-2 border-orange-500 inline-block">{{ detail.title }}</h3>
+    <div v-for="(detail, index) in props.details" :key="index" class="mt-4 p-2 flex flex-wrap justify-between items-baseline">
+        <h3 class="text-base font-bold mb-4 pb-1 border-b-2 border-orange-500 inline-block">{{ detail.title }}</h3>
         <p>{{ detail.price }}</p>
+        <p class="w-full text-xs">{{ detail.clarification }}</p>
     </div>
 </template>
