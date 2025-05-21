db2c5aa71b6b7553282c967849a76fcdff1ceef4
Alex <lexxik753@gmail.com>
2025-05-21 16:12:28 +0300

    start create feedback

---
+++ src/blocks/FeedBack.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="container mt-15 mx-auto px-4 bg-red-100">
+        <div class="text-center">
+            <h3 class="text-xl md:text-2xl lg:text-4xl font-bold">Свяжитесь с нами</h3>
+            <p class="text-base lg:text-lg xl:text-xl">Мы оперативно ответим на ваш запрос по телефону или электронной почте.</p>
+        </div>
+        <div>Сюда компонент формы!</div>
+    </section>
+</template>
--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -2,10 +2,12 @@
 import Header from './Header.vue';
 import About from '../blocks/About.vue';
 import Services from '../blocks/Services.vue';
+import FeedBack from '../blocks/FeedBack.vue';
 </script>
 
 <template>
     <Header />
     <About />
     <Services />
+    <FeedBack />
 </template>
