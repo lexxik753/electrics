587fbb7b83f3d94b6d27b82588c802b8d8fec0f0
Alexey Gurov <lexxik753@gmail.com>
2025-07-17 16:07:00 +0300

    feat: strat create component feedback

--- src/blocks/FeedBack.vue
+++ src/blocks/FeedBack.vue
@@ -1,13 +1,15 @@
 <script setup>
-
+import FeedbackForm from '../components/FeedbackForm.vue';
 </script>
 
 <template>
-    <section class="container mt-15 mx-auto px-4 bg-red-100">
+    <section class="container mt-15 mx-auto px-4">
         <div class="text-center">
-            <h3 class="text-xl md:text-2xl lg:text-4xl font-bold">Свяжитесь с нами</h3>
-            <p class="text-base lg:text-lg xl:text-xl">Мы оперативно ответим на ваш запрос по телефону или электронной почте.</p>
+            <h3 class="mb-5 mx-auto text-xl md:text-2xl lg:text-4xl font-bold">Свяжитесь с нами</h3>
+            <p class="mb-3 mx-auto text-base lg:text-lg xl:text-xl">Мы оперативно ответим на ваш запрос по телефону или электронной почте.</p>
         </div>
-        <div>Сюда компонент формы!</div>
+        <div>
+            <FeedbackForm />
+        </div>
     </section>
 </template>
---
+++ src/components/FeedbackForm.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+<template>
+    <div class="mt-4 flex flex-col md:grid grid-cols-2 grid-rows-1 gap-7">
+        <div class="place-self-center">
+            <p>8 800 123-45-67</p>
+            <p>example@example.ru</p>
+            <p>Бизнес-центр «Прогресс», ул. 34, Москва</p>
+        </div>
+        <div>
+            <form action="" class="flex flex-col w-2/3">
+                <input type="text" name="userEmail" placeholder="Ваш электронный адрес" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100"/>
+                <input type="text" name="userName" placeholder="Ваше полное имя"  class="mb-2 p-4 border-2 border-gray-300 bg-blue-100"/>
+                <input type="text" name="userComments" placeholder="Ваши комментарии" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100"/>
+                <button class="mb-2 p-4 w-1/2 rounded-full bg-blue-400">Оставить заявку</button>
+            </form>
+        </div>
+    </div>
+</template>
