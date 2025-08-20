f8be42cc6e3ade3bfd5a6ccd8d7372a398be93ff
Alexey Gurov <lexxik753@gmail.com>
2025-08-20 12:45:45 +0300

    fix: change text size, change form feedback

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -2,18 +2,18 @@
 
 </script>
 <template>
-    <div class="mt-4 flex flex-col md:grid grid-cols-2 grid-rows-1 gap-7">
-        <div class="place-self-center">
+    <div class="mt-4 flex flex-col md:grid grid-cols-2 grid-rows-1  gap-7">
+        <div class="place-self-center-safe lg:text-2xl/relaxed md:text-xl/loose ">
             <p>8 800 123-45-67</p>
             <p>example@example.ru</p>
             <p>Бизнес-центр «Прогресс», ул. 34, Москва</p>
         </div>
-        <div>
-            <form action="" class="flex flex-col w-2/3">
+        <div class="">
+            <form action="" class="flex flex-col w-full lg:w-2/3 sm:place-self-center">
                 <input type="text" name="userEmail" placeholder="Ваш электронный адрес" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100"/>
                 <input type="text" name="userName" placeholder="Ваше полное имя"  class="mb-2 p-4 border-2 border-gray-300 bg-blue-100"/>
-                <input type="text" name="userComments" placeholder="Ваши комментарии" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100"/>
-                <button class="mb-2 p-4 w-1/2 rounded-full bg-blue-400">Оставить заявку</button>
+                <textarea type="text" name="userComments" placeholder="Ваши комментарии" class="p-4 border-2 border-gray-300 bg-blue-100"/>
+                <button class="my-2 p-3 w-1/2 rounded-full bg-blue-400">Оставить заявку</button>
             </form>
         </div>
     </div>
