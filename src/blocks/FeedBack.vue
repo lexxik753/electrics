a35bcdb54a5367b17990de2d467af0bb1ffdd553
Alexey Gurov <lexxik753@gmail.com>
2025-08-20 15:38:01 +0300

    need accept style before continue

--- src/blocks/FeedBack.vue
+++ src/blocks/FeedBack.vue
@@ -4,10 +4,7 @@ import FeedbackForm from '../components/FeedbackForm.vue';
 
 <template>
     <section class="container mt-15 mx-auto px-4">
-        <div class="text-center">
-            <h3 class="mb-5 mx-auto text-xl md:text-2xl lg:text-4xl font-bold">Свяжитесь с нами</h3>
-            <p class="mb-3 mx-auto text-base md:text-xl xl:text-2xl">Мы оперативно ответим на ваш запрос по телефону или электронной почте.</p>
-        </div>
+        <h3 class="pb-5 text-xl md:text-2xl lg:text-4xl text-center font-bold">Свяжитесь с нами</h3>
         <div>
             <FeedbackForm />
         </div>
