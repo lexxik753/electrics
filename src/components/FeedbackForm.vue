649a0f481e2ae197971734a32356a37e011660d2
lexxik753 <lexxik753@gmail.com>
2025-08-24 20:21:38 +0300

    need fix feedback form

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -2,16 +2,12 @@
 
 </script>
 <template>
-    <div class="mt-4 shadow-2xl">
-        <form action="" class="pt-10 flex flex-wrap gap-3 w-full lg:w-1/3 sm:place-self-center">
-            <input type="text" name="userEmail" placeholder="Ваш электронный адрес"
-                class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
-            <input type="text" name="userPhone" placeholder="Ваш номер телефона"
-                class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
-            <input type="text" name="userName" placeholder="Ваше полное имя"
-                class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
-            <textarea type="text" name="userComments" placeholder="Ваши комментарии"
-                class="p-4 border-2 border-gray-300 bg-blue-100" />
+    <div class="mt-4 ">
+        <form action="" class="p-10 grid grid-cols-3 gap-3 w-full lg:w-2/3 sm:place-self-center shadow-2xl">
+            <input type="text" name="userEmail" placeholder="Ваш электронный адрес" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
+            <input type="text" name="userPhone" placeholder="Ваш номер телефона" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
+            <input type="text" name="userName" placeholder="Ваше полное имя" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
+            <textarea type="text" name="userComments" placeholder="Ваши комментарии" class="p-4 col-span-3 border-2 border-gray-300 bg-blue-100" />
             <button class="my-2 p-3 w-1/2 rounded-full bg-blue-400">Оставить заявку</button>
         </form>
     </div>
