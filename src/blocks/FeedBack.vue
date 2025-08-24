510840160b420de01e46396473121451634b190f
lexxik753 <lexxik753@gmail.com>
2025-08-24 19:10:23 +0300

    need fix...

--- src/blocks/FeedBack.vue
+++ src/blocks/FeedBack.vue
@@ -5,6 +5,9 @@ import FeedbackForm from '../components/FeedbackForm.vue';
 <template>
     <section class="container mt-15 mx-auto px-4">
         <h3 class="pb-5 text-xl md:text-2xl lg:text-4xl text-center font-bold">Свяжитесь с нами</h3>
+        <p class="text-center lg:text-2xl/relaxed md:text-xl/loose ">
+            Мы оперативно ответим на ваш запрос по телефону или электронной почте.
+        </p>
         <div>
             <FeedbackForm />
         </div>
--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -2,18 +2,17 @@
 
 </script>
 <template>
-    <div class="mt-4 flex flex-col md:grid grid-cols-2 grid-rows-1  gap-7">
-        <p class="place-self-center-safe text-center lg:text-2xl/relaxed md:text-xl/loose ">
-            Мы оперативно ответим на ваш запрос по телефону или электронной почте.
-        </p>
-        <div>
-            <form action="" class="flex flex-col w-full lg:w-2/3 sm:place-self-center">
-                <input type="text" name="userEmail" placeholder="Ваш электронный адрес" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
-                <input type="text" name="userPhone" placeholder="Ваш номер телефона" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
-                <input type="text" name="userName" placeholder="Ваше полное имя" class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
-                <textarea type="text" name="userComments" placeholder="Ваши комментарии" class="p-4 border-2 border-gray-300 bg-blue-100" />
-                <button class="my-2 p-3 w-1/2 rounded-full bg-blue-400">Оставить заявку</button>
-            </form>
-        </div>
+    <div class="mt-4 shadow-2xl">
+        <form action="" class="pt-10 flex flex-wrap gap-3 w-full lg:w-1/3 sm:place-self-center">
+            <input type="text" name="userEmail" placeholder="Ваш электронный адрес"
+                class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
+            <input type="text" name="userPhone" placeholder="Ваш номер телефона"
+                class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
+            <input type="text" name="userName" placeholder="Ваше полное имя"
+                class="mb-2 p-4 border-2 border-gray-300 bg-blue-100" />
+            <textarea type="text" name="userComments" placeholder="Ваши комментарии"
+                class="p-4 border-2 border-gray-300 bg-blue-100" />
+            <button class="my-2 p-3 w-1/2 rounded-full bg-blue-400">Оставить заявку</button>
+        </form>
     </div>
 </template>
